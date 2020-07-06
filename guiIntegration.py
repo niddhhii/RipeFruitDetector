02aed50ed37afa0a30a5284ed4b1b8c12fc71a3a
Esha <esha.gupta@somaiya.edu>
2020-07-07 01:23:46 +0530

    Integrated GUI

--- guiIntegration.py
+++ guiIntegration.py
@@ -20,6 +20,7 @@ def img_open():
     show_res_btn = Button(canvas, text = "Show Results", command = show_results)
     show_res_btn.place(relx = 0.5, rely = 0.8, relwidth = 0.30, relheight = 0.05, anchor = CENTER)
 
+#FUNCTION FOR DROPDOWN OPTION
 def dropdown_click(event):
     global selected
     selected = dropdown.get()
@@ -29,33 +30,75 @@ def show_results():
         banana()
     elif selected == 'Orange':
         orange()
+    elif selected == 'Papaya':
+        papaya()
     show_res = Label(canvas, text=result, font = ('Times New Roman', '15'), bg = 'white')
     show_res.place(relx = 0.5, rely = 0.9, relwidth = 0.50, relheight = 0.05, anchor = CENTER)
 
-def banana():
-    global result
-    hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
-
-    mask_green = cv2.inRange(hsv, (36, 0, 0), (70, 255,255))
+#FUNCTION FOR GREEN MASK VALUES
+def GreenMask(OImage):
+    mask_green = cv2.inRange(OImage, (36, 0, 0), (70, 255,255))
+    #cv2.imshow('Green',mask_green)
     count_green = 0
     for g in mask_green:
         count_green = count_green + list(g).count(255)
     print("Green",count_green)
 
-    mask_yellow = cv2.inRange(hsv, (15,0,0), (36, 255, 255))
+    return mask_green, count_green
+
+#FUNCTION FOR YELLOW MASK VALUES
+def YellowMask(OImage):
+    mask_yellow = cv2.inRange(OImage, (15,0,0), (36, 255, 255))
     count_yellow = 0
     for y in mask_yellow:
         count_yellow = count_yellow + list(y).count(255)
     print("Yellow",count_yellow)
 
-    brown1 = cv2.inRange(hsv, (0,100,20), (17,255,255))
-    brown2 = cv2.inRange(hsv, (170,100,20), (180,255,255))
+    return mask_yellow, count_yellow
+
+#FUNCTION FOR BROWN MASK VALUES
+def BrownMask(OImage):
+    brown1 = cv2.inRange(OImage, (0,100,20), (17,255,255))
+    brown2 = cv2.inRange(OImage, (170,100,20), (180,255,255))
     mask_brown = cv2.bitwise_or(brown1, brown2)
     # cv2.imshow('Brown', mask_brown)
     count_brown = 0
     for br in mask_brown:
         count_brown = count_brown + list(br).count(255)
     print("Brown",count_brown)
+    return mask_brown, count_brown
+
+#FUNCTION FOR ORANGE MASK VALUES
+def OrangeMask(OImage):
+    mask_orange = cv2.inRange(OImage, (10, 100, 20), (20, 255,255))
+    mask2 = cv2.inRange(OImage, (11, 40, 215), (179, 255,255))
+    count_orange = 0
+    for g in mask_orange:
+        count_orange = count_orange + list(g).count(255)
+    print("Orange",count_orange)
+    return mask_orange, count_orange
+
+#FUNCTION FOR RED MASK VALUES
+def RedMask(OImage):
+    red1 = cv2.inRange(hsv, (0,120,25), (10, 255, 255))
+    red2 = cv2.inRange(hsv, (170,120,70), (180, 255, 255))
+    mask_red = cv2.bitwise_or(red1,red2)
+    count_red = 0
+    for y in mask_red:
+        count_red= count_red+ list(y).count(255)
+    print("Red",count_red)
+    return mask_red, count_red
+
+#FUNCTION FOR BANANA
+def banana():
+    global result
+    hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+
+    mask_green,count_green = GreenMask(hsv)
+
+    mask_yellow, count_yellow = YellowMask(hsv)
+
+    mask_brown, count_brown = BrownMask(hsv)
 
     mask_final = mask_green + mask_yellow + mask_brown
 
@@ -87,44 +130,24 @@ def banana():
         result = "Result : Ripe (Scale:3)"
         print('Ripe')
     elif brpercent > 0.8:
-        result = "Result : Fully Ripe (Scale:5)"
+        result = "Result : Over Ripe (Scale:5)"
         print('Over Ripe')
     else:
         result = "Result : Unripe (Scale:2)"
         print('Unripe')
 
-
+#FUNCTION FOR ORANGE
 def orange():
     global result
     hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 
-    mask_green = cv2.inRange(hsv, (36, 0, 0), (70, 255,255))
-    count_green = 0
-    for g in mask_green:
-        count_green = count_green + list(g).count(255)
-    print("Green",count_green)
+    mask_green,count_green = GreenMask(hsv)
 
-    mask_orange = cv2.inRange(hsv, (10, 100, 20), (20, 255,255))
-    mask2 = cv2.inRange(hsv, (11, 40, 215), (179, 255,255))
-    count_orange = 0
-    for g in mask_orange:
-        count_orange = count_orange + list(g).count(255)
-    print("Orange",count_orange)
+    mask_yellow, count_yellow = YellowMask(hsv)
 
-    mask_yellow = cv2.inRange(hsv, (15,0,0), (36, 255, 255))
-    count_yellow = 0
-    for y in mask_yellow:
-        count_yellow = count_yellow + list(y).count(255)
-    print("Yellow",count_yellow)
+    mask_brown, count_brown = BrownMask(hsv)
 
-    brown1 = cv2.inRange(hsv, (0,100,20), (17,255,255))
-    brown2 = cv2.inRange(hsv, (170,100,20), (180,255,255))
-    mask_brown = cv2.bitwise_or(brown1, brown2)
-
-    count_brown = 0
-    for br in mask_brown:
-        count_brown = count_brown + list(br).count(255)
-    print("Brown",count_brown)
+    mask_orange, count_orange = OrangeMask(hsv)
 
     mask_final = mask_green + mask_orange + mask_yellow + mask_brown
 
@@ -164,6 +187,69 @@ def orange():
     else:
         print('NULL')
 
+#FUNCTION FOR PAPAYA
+def papaya():
+    global result
+    # Convert to hsv
+    hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+
+    mask_green,count_green = GreenMask(hsv)
+    #cv2.imshow('Green',mask_green)
+
+    mask_yellow, count_yellow = YellowMask(hsv)
+    #cv2.imshow('Yellow',mask_yellow)
+
+    
+    mask_red, count_red = RedMask(hsv)
+    #cv2.imshow('Red',mask_red)
+
+    mask_brown, count_brown = BrownMask(hsv)
+    #cv2.imshow('Brown',mask_brown)
+
+    # Final Mask
+    mask_final = mask_green + mask_yellow + mask_red + mask_brown
+
+
+    res = cv2.bitwise_and(original,original, mask=mask_final)
+    contours, hierarchy = cv2.findContours(mask_final,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    area = [cv2.contourArea(x) for x in contours]
+    max_index = np.argmax(area)
+    maxcontour = contours[max_index]
+    cv2.drawContours(original,[maxcontour],-1,(0,255,0),10)
+    #cv2.imshow('Frame1',original)
+
+    # Calculate the total area
+    total_count = count_green + count_yellow + count_red + count_brown
+    print(total_count)
+
+    # Calculate % of each colour
+    gpercent = count_green/total_count
+    ypercent = count_yellow/total_count
+    rpercent = count_red/total_count
+    brpercent = count_brown/total_count
+    print(gpercent)
+    print(ypercent)
+    print(rpercent)
+    print(brpercent)
+
+    # Conditions for Papaya fruit scaling
+    if gpercent > 0.75:
+        result = "Result : Raw (Scale:1)"
+        print('Raw')
+    elif ypercent > 0.65 and gpercent < 0.1 and brpercent < 0.2:
+        result = "Result : Ripe (Scale:3)"
+        print('Ripe')
+    elif brpercent < 0.3 and ypercent < 0.8 and gpercent<0.1:
+        result = "Result : Very Ripe (Scale:4)"
+        print('Very Ripe')
+    elif ypercent > 0.5 and gpercent < 0.3 and brpercent<0.1:
+        result = "Result : Unripe (Scale:2)"
+        print('Unripe')
+    elif brpercent > 0.3:
+        result = "Result : Over Ripe (Scale:5)"
+        print('Over Ripe')
+
+
 
 ##################GUI
 root = tk.Tk()
@@ -177,9 +263,10 @@ label.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 
 options = [
     "Select Fruit",
+    "Apple",
     "Banana",
     "Orange",
-    "Apple"
+    "Papaya"
 ]
 
 dropdown = TTK.Combobox(canvas, value=options, font = ('Consolas', '13'))
