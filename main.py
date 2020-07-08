cc1073e011cf5302df07233540272b47cf9abfe3
niddhhii <dedhianidhi21@gmail.com>
2020-07-08 14:55:02 +0530

    Updated final code

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ def show_results():
         orange()
     elif selected == 'Papaya':
         papaya()
+    elif selected == 'Apple':
+        apple()
     show_res = Label(canvas, text=result, font = ('Times New Roman', '15'), bg = 'white')
     show_res.place(relx = 0.5, rely = 0.9, relwidth = 0.50, relheight = 0.05, anchor = CENTER)
 
@@ -45,7 +47,6 @@ def GreenCount(mask_green):
     count_green = 0
     for g in mask_green:
         count_green = count_green + list(g).count(255)
-    # print("Green",count_green)
     return count_green
 
 # Function for yellow count
@@ -53,7 +54,6 @@ def YellowCount(mask_yellow):
     count_yellow = 0
     for y in mask_yellow:
         count_yellow = count_yellow + list(y).count(255)
-    # print("Yellow",count_yellow)
     return count_yellow
 
 # Function for brown count
@@ -61,7 +61,6 @@ def BrownCount(mask_brown):
     count_brown = 0
     for br in mask_brown:
         count_brown = count_brown + list(br).count(255)
-    # print("Brown",count_brown)
     return count_brown
 
 # Function for orange count
@@ -69,8 +68,14 @@ def OrangeCount(mask_orange):
     count_orange = 0
     for g in mask_orange:
         count_orange = count_orange + list(g).count(255)
-    # print("Orange",count_orange)
     return count_orange
+
+# Function for red count
+def RedCount(mask_red):
+    count_red = 0
+    for y in mask_red:
+        count_red= count_red+ list(y).count(255)
+    return count_red
 
 # Function for finding and drawing contours
 def contour(mask_final):
@@ -85,36 +90,35 @@ def contour(mask_final):
 # Function for scaling banana fruit
 def banana():
     global result
+
+    # Converting bgr image to hsv
     hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 
+    # Green Color Mask
     mask_green = cv2.inRange(hsv, (36, 30, 0), (70, 255,255))
-    # cv2.imshow('Green', mask_green)
     count_green = GreenCount(mask_green)
 
+    # Yellow Color Mask
     mask_yellow = cv2.inRange(hsv, (15,60,0), (36, 255, 255))
-    # cv2.imshow('Yellow', mask_yellow)
     count_yellow = YellowCount(mask_yellow)
 
+    # Brown Color Mask
     brown1 = cv2.inRange(hsv, (0,100,0), (17,255,255))
     brown2 = cv2.inRange(hsv, (165,100,20), (180,255,255))
     mask_brown = cv2.bitwise_or(brown1, brown2)
-    # cv2.imshow('Brown', mask_brown)
     count_brown = BrownCount(mask_brown)
 
+    # Final Mask Calculation
     mask_final = mask_green + mask_yellow + mask_brown
-
     contour(mask_final)
 
+    # Calculating total area
     total_count = count_green + count_yellow + count_brown
-    # print(total_count)
-
     gpercent = count_green/total_count
     ypercent = count_yellow/total_count
     brpercent = count_brown/total_count
-    # print(gpercent)
-    # print(ypercent)
-    # print(brpercent)
 
+    # Conditions for scaling of banana
     if gpercent > 0.8:
         result = "Result : Raw (Scale:1)"
         print('Raw')
@@ -138,38 +142,27 @@ def orange():
     hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 
     mask_green = cv2.inRange(hsv, (36, 50, 0), (70, 255,255))
-    # cv2.imshow('Green', mask_green)
     count_green = GreenCount(mask_green)
 
     mask_yellow = cv2.inRange(hsv, (21,60,0), (36, 255, 255))
-    # cv2.imshow('Yellow', mask_yellow)
     count_yellow = YellowCount(mask_yellow)
 
     brown1 = cv2.inRange(hsv, (0,50,0), (9,255,255))
     brown2 = cv2.inRange(hsv, (165,100,20), (180,255,255))
     mask_brown = cv2.bitwise_or(brown1, brown2)
-    # cv2.imshow('Brown', mask_brown)
     count_brown = BrownCount(mask_brown)
     
     mask_orange = cv2.inRange(hsv, (10, 50, 20), (20, 255,255))
-    # cv2.imshow('Orange', mask_orange)
     count_orange = OrangeCount(mask_orange)
 
     mask_final = mask_green + mask_orange + mask_yellow + mask_brown
-
     contour(mask_final)
 
     total_count = count_green + count_yellow + count_orange + count_brown
-    # print(total_count)
-
     gpercent = count_green/total_count
     opercent = count_orange/total_count
     ypercent = count_yellow/total_count
     brpercent = count_brown/total_count
-    # print(gpercent)
-    # print(opercent)
-    # print(ypercent)
-    # print(brpercent)
 
     if gpercent > 0.75:
         result = "Result : Raw (Scale:1)"
@@ -194,32 +187,23 @@ def papaya():
     hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 
     mask_green = cv2.inRange(hsv, (36, 30, 0), (70, 255,255))
-    # cv2.imshow('Green', mask_green)
     count_green = GreenCount(mask_green)
 
     mask_yellow = cv2.inRange(hsv, (15,80,0), (36, 255, 255))
-    # cv2.imshow('Yellow', mask_yellow)
     count_yellow = YellowCount(mask_yellow)
 
     brown1 = cv2.inRange(hsv, (0,100,0), (15,255,255))
     brown2 = cv2.inRange(hsv, (165,100,20), (180,255,255))
     mask_brown = cv2.bitwise_or(brown1, brown2)
-    # cv2.imshow('Brown', mask_brown)
     count_brown = BrownCount(mask_brown)
     
     mask_final = mask_green + mask_yellow + mask_brown
-
     contour(mask_final)
 
     total_count = count_green + count_yellow + count_brown
-    # print(total_count)
-
     gpercent = count_green/total_count
     ypercent = count_yellow/total_count
     brpercent = count_brown/total_count
-    # print(gpercent)
-    # print(ypercent)
-    # print(brpercent)
 
     if gpercent > 0.75:
         result = "Result : Raw (Scale:1)"
@@ -244,32 +228,23 @@ def mango():
     hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 
     mask_green = cv2.inRange(hsv, (33, 20, 0), (70, 255,255))
-    # cv2.imshow('Green', mask_green)
     count_green = GreenCount(mask_green)
 
     mask_yellow = cv2.inRange(hsv, (13,60,0), (33, 255, 255))
-    # cv2.imshow('Yellow', mask_yellow)
     count_yellow = YellowCount(mask_yellow)
 
     brown1 = cv2.inRange(hsv, (0,20,20), (13,255,255))
     brown2 = cv2.inRange(hsv, (170,100,20), (180,255,255))
     mask_brown = cv2.bitwise_or(brown1, brown2)
-    # cv2.imshow('Brown', mask_brown)
     count_brown = BrownCount(mask_brown)
 
     mask_final = mask_green + mask_yellow + mask_brown
-
     contour(mask_final)
 
     total_count = count_green + count_yellow + count_brown
-    # print(total_count)
-
     gpercent = count_green/total_count
     ypercent = count_yellow/total_count
     brpercent = count_brown/total_count
-    # print(gpercent)
-    # print(ypercent)
-    # print(brpercent)
 
     if gpercent > 0.75:
         result = "Result : Raw (Scale:1)"
@@ -286,6 +261,54 @@ def mango():
     elif ypercent > 0.80:
         result = "Result : Ripe (Scale:3)"
         print('Ripe')
+
+
+# Function for scaling mango fruit
+def apple():
+    global result
+    hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+        
+    red1 = cv2.inRange(hsv, (0,90,25), (10, 255, 255))
+    red2 = cv2.inRange(hsv, (170,120,70), (180, 255, 255))
+    mask_red = cv2.bitwise_or(red1,red2)
+    count_red = RedCount(mask_red)
+
+    brown1 = cv2.inRange(hsv, (10,90,10), (15,255,255))
+    brown2 = cv2.inRange(hsv, (165,100,20), (170,255,255))
+    mask_brown = cv2.bitwise_or(brown1, brown2)
+    count_brown = BrownCount(mask_brown)
+
+    mask_green = cv2.inRange(hsv, (36, 70, 0), (70, 255,255))
+    count_green = GreenCount(mask_green)
+
+    mask_yellow = cv2.inRange(hsv, (15,80,15), (36, 255, 255))
+    count_yellow = YellowCount(mask_yellow)
+
+    mask_final = mask_green + mask_yellow +  mask_red + mask_brown
+    contour(mask_final)
+
+    total_count = count_green + count_yellow +  count_red + count_brown
+    gpercent = count_green/total_count
+    ypercent = count_yellow/total_count
+    brpercent = count_brown/total_count
+    rpercent = count_red/total_count
+
+    if gpercent > 0.75:
+        result = "Result : Raw (Scale:1)"
+        print('Raw')
+    elif rpercent > 0.8:
+        result = "Result : Ripe (Scale:3)"
+        print('Ripe')
+    elif rpercent > 0.45 and gpercent < 0.1 and brpercent > 0.4:
+        result = "Result : Very Ripe (Scale:4)"
+        print('Very Ripe')
+    elif brpercent > 0.2 and rpercent < 0.2 and ypercent < 0.6:
+        result = "Result : Over Ripe (Scale:5)"
+        print('Over Ripe')
+    elif rpercent < 0.7 and ypercent > 0.15 and brpercent < 0.3 :
+        result = "Result : Unripe (Scale:2)"
+        print('Unripe')
+    
 
 
 #GUI
